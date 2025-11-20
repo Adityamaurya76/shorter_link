f84e4a80cffb351ae26fc37d529439d5175bcd7c
Aditya Maurya <aditya@hipster-dev.com>
2025-11-20 14:34:39 +0800

    fix error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 const port = process.env.PORT;
 
 connectDB().then(() => {
-   app.listen(port, () => {
+   app.listen(port, '0.0.0.0', () => {
        console.log(`Server is running on port ${port}`);
    });
 }).catch((error) => {
