ccd9cc8044285f8d4428e220439299a748f2347d
Aditya Maurya <aditya@hipster-dev.com>
2025-11-20 16:56:42 +0800

    update healthcheck api

--- src/controllers/health.controllers.js
+++ src/controllers/health.controllers.js
@@ -1,15 +1,7 @@
 import { asyncHandler } from "../utils/async-handler.js";
-import os from "os";
 
-const healthCheck = asyncHandler(async (req, res) => {
-    const uptime = process.uptime();
-    const healthData = {
-        ok: true,
-        version: "1.0",
-        uptime: Math.floor(uptime),
-        timestamp: new Date().toISOString(),
-    };
-    res.status(200).json(healthData);
+const healthCheck =  asyncHandler(async (req, res) => {
+    res.status(200).json(new ApiResponse(200, null, "Server is running"));
 });
 
 export { healthCheck };
